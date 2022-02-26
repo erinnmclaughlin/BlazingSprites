0e3a7f3c9a5582d2429007b7d532b0c7a8ec807f
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-02-26 15:42:22 -0500

    Add project files.

---
+++ BlazingSprites/Models/Character/CharacterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazingSprites.Models.Character;
+
+public class CharacterModel
+{
+    public HairStyle HairStyle { get; set; }
+    public HairColor HairColor { get; set; }
+    public SkinColor SkinColor { get; set; }
+}
---
+++ BlazingSprites/Models/Character/CustomizationOptions.cs
@@ -0,0 +1,5 @@
+﻿namespace BlazingSprites.Models.Character;
+
+public enum HairColor { DarkBrown, Blonde, Brown, LightBrown, Ginger, Green, LimeGreen, Gray, LightPurple, Purple, Pink, Violet, Red, Teal }
+public enum HairStyle { Bob, Braids, Buzzcut, Curly, Emo, ExtraLong, ExtraLongSkirt, FrenchCurl, Gentleman, LongStraight, LongStraightSkirt, Midiwave, Ponytail, Spacebuns, Wavy }
+public enum SkinColor { Skin1, Skin2, Skin3, Skin4, Skin5, Skin6, Skin7, Skin8 }
