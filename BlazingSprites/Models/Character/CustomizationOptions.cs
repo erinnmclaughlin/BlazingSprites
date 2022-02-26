52c614538abb399648bafa731b24de2e17dc1844
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-02-26 18:56:32 -0500

    add none as a hair option

--- BlazingSprites/Models/Character/CustomizationOptions.cs
+++ BlazingSprites/Models/Character/CustomizationOptions.cs
@@ -4,7 +4,7 @@ public enum HairColor { Black, Blonde, Brown, LightBrown, Copper, Emerald, Green
 public enum EyeColor { Style1, Style2, Style3, Style4, Style5, Style6, Style7, Style8, Style9, Style10, Style11, Style12, Style13, Style14 };
 
 public enum BlushColor { Style1, Style2, Style3, Style4, Style5 };
-public enum HairStyle { Bob, Braids, Buzzcut, Curly, Emo, ExtraLong, ExtraLongSkirt, FrenchCurl, Gentleman, LongStraight, LongStraightSkirt, Midiwave, Ponytail, Spacebuns, Wavy }
+public enum HairStyle { None, Bob, Braids, Buzzcut, Curly, Emo, ExtraLong, ExtraLongSkirt, FrenchCurl, Gentleman, LongStraight, LongStraightSkirt, Midiwave, Ponytail, Spacebuns, Wavy }
 public enum LipColor { Style1, Style2, Style3, Style4, Style5 };
 public enum SkinColor { Skin1, Skin2, Skin3, Skin4, Skin5, Skin6, Skin7, Skin8 }
 
