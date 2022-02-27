ad2558786365cdf07b76ae86ec5898a301854a5c
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-02-26 19:47:59 -0500

    cleanup stuff

--- BlazingSprites/Models/Character/CustomizationOptions.cs
+++ BlazingSprites/Models/Character/CustomizationOptions.cs
@@ -5,11 +5,11 @@ public enum Direction { Down, Up, Left, Right }
 public enum HairColor { Black, Blonde, Brown, LightBrown, Copper, Emerald, Green, Grey, Lilac, Navy, Pink, Purple, Red, Turquoise }
 public enum HairStyle { None, Bob, Braids, Buzzcut, Curly, Emo, Extra_Long, French_Curl, Gentleman, Long_Straight, Midiwave, Ponytail, Spacebuns, Wavy }
 
-public enum ClothesColor { Style1, Style2, Style3, Style4, Style5, Style6, Style7, Style8, Style9, Style10 };
+public enum ClothesColor { Color1, Color2, Color3, Color4, Color5, Color6, Color7, Color8, Color9, Color10 };
 public enum ShirtStyle { Basic, Dress, Floral, Overalls, Sailor, Sailor_Bow, Skull, Spaghetti, Sporty, Stripe, Suit }
 public enum PantStyle { Pants, Pants_Suit, Skirt }
 
-public enum BlushColor { None, Style1, Style2, Style3, Style4, Style5 };
-public enum EyeColor { Style1, Style2, Style3, Style4, Style5, Style6, Style7, Style8, Style9, Style10, Style11, Style12, Style13, Style14 };
-public enum LipColor { Style1, Style2, Style3, Style4, Style5 };
+public enum BlushColor { None, Color1, Color2, Color3, Color4, Color5 };
+public enum EyeColor { Color1, Color2, Color3, Color4, Color5, Color6, Color7, Color8, Color9, Color10, Color11, Color12, Color13, Color14 };
+public enum LipColor { Color1, Color2, Color3, Color4, Color5 };
 public enum SkinColor { Skin1, Skin2, Skin3, Skin4, Skin5, Skin6, Skin7, Skin8 }
